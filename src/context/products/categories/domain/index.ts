2d7165cff232ddb59e8c7cd2bd27b5267ae4a7fd
Ange6506 <angelicacb6392@gmail.com>
2026-07-08 17:44:42 -0500

    feat(categorias): agregar búsqueda y filtrado de categorías

--- src/context/products/categories/domain/index.ts
+++ src/context/products/categories/domain/index.ts
@@ -1,4 +1,3 @@
 export * from "./product-category.aggregate";
 export * from "./product-category.repository";
-export * from "./product-category-created.event";
 export * from "./product-category-not-found.exception";
