0cd89144215048ba4d6438a077787170d57d1300
Ange6506 <angelicacb6392@gmail.com>
2026-07-21 15:08:13 -0500

    refactor: eliminar los contextos de receta

--- src/context/recipes/items/application/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './use-cases/create-item/create-recipe-item.use-case';
-export * from './use-cases/update-item/update-recipe-item.use-case';
-export * from './use-cases/search-items/search-recipe-item.use-case';
--- src/context/recipes/items/domain/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./recipe-item-primitives";
-export * from "./recipe-item-response";
-export * from "./recipe-item-application";
